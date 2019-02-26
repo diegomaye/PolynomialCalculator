0d782a94001a90ad0f99952911031b41c3acba3f
Rodrigo Olivera <ro.olivera@gmail.com>
2019-02-26 05:05:35 -0300

    ISSUE#5 se implementa Polinomios

--- PolynomialCalculator/Polinomios.h
+++ PolynomialCalculator/Polinomios.h
@@ -10,5 +10,35 @@
 #define Polinomios_h
 
 #include <stdio.h>
+#include "Polinomio.h"
+
+typedef struct nodo_polinomio {
+    			Polinomio pol;
+    			nodo_polinomio * hizq;
+    			nodo_polinomio * hder;
+                } Nodo;
+
+typedef Nodo * ABBPolinomios;
+
+void insertarPolinomio (ABBPolinomios &arbolPolinomios, Polinomio polinomio);
+//inserta un polinomio en el árbol binario de búsqueda de polinomios
+Boolean existeAlMenosUnPolinomioEnABB(ABBPolinomios arbolPolinomios);
+//devuelve si existe al menos un polinomio en memoria
+Boolean existePolinomioEnABB(ABBPolinomios arbolPolinomios, String nombre);
+//devuelve si existe un polinomio con ese nombre
+void desplegarABB(ABBPolinomios arbolPolinomios);
+//desplega el arbol binario de búsqueda de polinomios por pantalla
+Polinomio darRaiz(ABBPolinomios arbolPolinomios);
+//PRECONDICION:arbolPolinomios no es vacío - Devuelve la raíz del árbol
+ABBPolinomios darIzquierdo(ABBPolinomios arbolPolinomios);
+//PRECONDICION:arbolPolinomios no es vacío - Devuelve el hijo izquierdo del árbol
+ABBPolinomios darDerecho(ABBPolinomios arbolPolinomios);
+//PRECONDICION:arbolPolinomios no es vacío - Devuelve el hijo derecho del árbol
+void eliminarABB(ABBPolinomios &arbolPolinomios);
+//elimina el arbol binario de búsqueda de polinomios en memoria
+Polinomio buscarPolinomio(ABBPolinomios arbol, String nombre);
+//devuelve si existe un polinomio con ese nombre
+/*PRECONDICION:el polinomio con ese nombre debe existir en arbol*/
+
 
 #endif /* Polinomios_h */

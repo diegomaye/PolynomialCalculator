b4e6c79a91e20920952a6edd206fb0dd7d17eea7
Rodrigo Olivera <ro.olivera@gmail.com>
2019-03-01 00:56:53 -0300

    TestPolinomios added.

---
+++ PolynomialCalculator/TestPolinomios.cpp
@@ -0,0 +1,20 @@
+#include "TestPolinomios.h"
+
+void pruebaInsertarPolinomio() {
+}
+void pruebaExisteAlMenosUnPolinomioEnABB() {
+}
+void pruebaExistePolinomioEnABB() {
+}
+void pruebaDesplegarABB() {
+}
+void pruebaDarRaiz() {
+}
+void pruebaDarIzquierdo() {
+}
+void pruebaDarDerecho() {
+}
+void pruebaEliminarABB() {
+}
+void pruebaBuscarPolinomio() {
+}
---
+++ PolynomialCalculator/TestPolinomios.h
@@ -0,0 +1,18 @@
+#ifndef TESTPOLINOMIOS_H_INCLUDED
+#define TESTPOLINOMIOS_H_INCLUDED
+
+#include <stdio.h>
+#include "Polinomios.h"
+
+
+void pruebaInsertarPolinomio();
+void pruebaExisteAlMenosUnPolinomioEnABB();
+void pruebaExistePolinomioEnABB();
+void pruebaDesplegarABB();
+void pruebaDarRaiz();
+void pruebaDarIzquierdo();
+void pruebaDarDerecho();
+void pruebaEliminarABB();
+void pruebaBuscarPolinomio();
+
+#endif // TESTPOLINOMIOS_H_INCLUDED

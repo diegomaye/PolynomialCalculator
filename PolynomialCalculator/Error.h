9e825884df63373c39114cdf0f1519890d73369b
Rodrigo Olivera <ro.olivera@gmail.com>
2019-03-10 22:08:55 -0300

    Messages with accents fixed

--- PolynomialCalculator/Error.h
+++ PolynomialCalculator/Error.h
@@ -44,29 +44,29 @@ typedef struct { Error value; char * descripcion; } Mensaje;
 
 const Mensaje listaErrores[] = {
     {FORMATO_POLINOMIO_INCORRECTO, "El formato del nombre del polinomio no es correcto.\n"},
-    {STRING_ALFANUMERICO, "El nombre del Polinomio debe ser alfanumérico"},
-    {STRING_ALFANUMERICO1, "El nombre del Polinomio del primer parametro debe ser alfanumérico"},
-    {CANTIDAD_PARAMETROS, "Cantidad de parámetros incorrecta.\n"},
+    {STRING_ALFANUMERICO, "El nombre del Polinomio debe ser alfanumerico"},
+    {STRING_ALFANUMERICO1, "El nombre del Polinomio del primer parametro debe ser alfanumerico"},
+    {CANTIDAD_PARAMETROS, "Cantidad de parametros incorrecta.\n"},
     {NOMBRE_INCORRECTO, "El formato del nombre del polinomio no es correcto.\n"},
     {POLINOMIO_EXISTENTE, "El polinomio ha ser creado ya existe.\n"},
     {COEFICIENTE_CERO, "El primer coeficiente no puede ser cero.\n"},
-    {FALTA_NOMBRE, "Cantidad de parámetros incorrecta.\n"},
+    {FALTA_NOMBRE, "Cantidad de parametros incorrecta.\n"},
     {POLINOMIO_NO_EXISTENTE, "Polinomio ingresado no existe en el ABB.\n"},
-    {POLINOMIO_NO_EXISTENTE1, "El nombre del Polinomio del segundo parametro debe ser alfanumérico.\n"},
-    {POLINOMIO_NO_EXISTENTE2, "El nombre del Polinomio del tercer parametro debe ser alfanumérico.\n"},
-    {COEFICIENTE_NUMERICOS, "Los coeficientes del polinomio tiene que ser números.\n"},
-    {CANTIDAD_PARAMETROS_MULTILICACION, "La multiplicación debe tener 3 parámetros.\n"},
+    {POLINOMIO_NO_EXISTENTE1, "El nombre del Polinomio del segundo parametro debe ser alfanumerico.\n"},
+    {POLINOMIO_NO_EXISTENTE2, "El nombre del Polinomio del tercer parametro debe ser alfanumerico.\n"},
+    {COEFICIENTE_NUMERICOS, "Los coeficientes del polinomio tiene que ser numeros.\n"},
+    {CANTIDAD_PARAMETROS_MULTILICACION, "La multiplicacion debe tener 3 parametros.\n"},
     {POLINOMIOS_EXISTENTE_MULTIPLICACION, "Los polinomios a multiplicar deben de existir en el ABB de Polinomios.\n"},
-    {VALOR_PARA_EVALUAR, "El valor a evaluar debe ser numérico.\n"},
+    {VALOR_PARA_EVALUAR, "El valor a evaluar debe ser numerico.\n"},
     {RESULTADO_EVALUAR_ERROR, "No es raiz.\n"},
     {RESULTADO_EVALUAR_CORRECTO, "Si es raiz.\n"},
-    {POLINOMIO_NO_EXISTENTE_EN_MEMORIA, "No existe ningún polinomio en memoria.\n"},
+    {POLINOMIO_NO_EXISTENTE_EN_MEMORIA, "No existe ningun polinomio en memoria.\n"},
     {FORMATO_ARCHIVO_INCORRECTO, "El formato del nombre del archivo no es correcto.\n"},
-    {NOMBRE_EXTENSION_INCORRECTO, "El nombre o la extensión no es correcto.\n"},
+    {NOMBRE_EXTENSION_INCORRECTO, "El nombre o la extension no es correcto.\n"},
     {EXISTE_ARCHIVO, "Desea sobreescribir el archivo?"},
     {NO_EXISTE_ARCHIVO, "No existe un archivo en el disco con el nombre ingresado.\n"},
     {COMANDO_INGRESADO_NO_EXISTE, "El comando ingresado es incorrecto.\n"}
-    
+
 };
 
 char* mostrarError(Error error);
